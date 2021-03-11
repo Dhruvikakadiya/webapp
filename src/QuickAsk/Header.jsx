f0d9a7cf67b4483ad27292ed8cd2a440c5a485c1
Dhruvi kakadiya <kakadiyadhruvi1700@gmail.com>
2021-03-11 15:14:32 +0530

    implemented header and footer

---
+++ src/QuickAsk/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                Quick Ask
+            </div>
+        </>
+
+    );
+}
+
+export default Header;
