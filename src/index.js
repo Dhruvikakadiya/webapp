af02d8e9ad04105ca701362837110dad1c974585
jhkblog <jemishakoladiya2001@gmail.com>
2021-03-18 11:45:39 +0530

    hello file is delete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
+import App from './App'
 import Department from './QuickAsk/componet1/companyprofile/Department'
 import Comproform from './QuickAsk/componet1/companyprofile/Comproform'
 import reportWebVitals from './reportWebVitals';
 import Comprofile from './QuickAsk/componet1/companyprofile/Comprofile';
+import Step1 from './QuickAsk/NEWINTERVIEW/STEPS/Step1';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Department/>
-    <Comprofile/>
+    {/* <Department/>
+    <Comprofile/> */}
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
